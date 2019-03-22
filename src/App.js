e7c54989bee7948f982e817038a2a6651c9679ed
Katie Macaulay <katiemacaulay@Katies-MacBook-Pro.local>
2019-03-21 19:30:30 -0500

    react-router practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import TopNav from "./components/TopNav";
 import SideNav from "./components/SideNav";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+let Tables = () => <div>Table Stuff</div>;
+let Charts = () => <div>Chart Stuff</div>;
+let Things = () => <div>Things</div>;
 
 function App() {
   return (
-    <div> 
-      <div id="wrapper">
-        <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-          <TopNav />
-          <SideNav />
-        </nav>
-        <div style={{backgroundColor: "white"}}>
-          {/* PUT YOUR ROUTES HERE */}
+    <Router>
+      <div> 
+        <div id="wrapper">
+          <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <TopNav />
+            <SideNav />
+          </nav>
+          <div style={{backgroundColor: "white"}}>
+           <Route exact path="/" component={Dashboard} />
+           <Route exact path="/things" component={Things} />
+           <Route path="/charts" component={Charts} />
+           <Route path="/tables" component={Tables} />
+          </div>
         </div>
       </div>
-    </div>
-
+    </Router>
   );
 }
 
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,27 +1,33 @@
 import React from "react";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 function SideNav() {
   return (
     <div className="collapse navbar-collapse navbar-ex1-collapse">
       <ul className="nav navbar-nav side-nav">
         <li className="active">
-          {/*
+          
           <Link to="/"> <i className="fa fa-fw fa-dashboard" />
             Dashboard 
           </Link>
-          */}
+         
         </li>
         <li>
-          <a href="charts.html">
-            <i className="fa fa-fw fa-bar-chart-o" /> Charts
-          </a>
+        <Link to="/charts"> <i className="fa fa-fw fa-dashboard" />
+            CHarts
+          </Link>
         </li>
         <li>
-          <a href="tables.html">
-            <i className="fa fa-fw fa-table" /> Tables
-          </a>
+        <Link to="/Tables"> <i className="fa fa-fw fa-dashboard" />
+            Tables
+          </Link>
         </li>
+        <li>
+        <Link to="/Things"> <i className="fa fa-fw fa-dashboard" />
+            Things
+          </Link>
+        </li>
+
       </ul>
     </div>);  
 }
